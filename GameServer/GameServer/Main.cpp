12024296d047c6b173b33c82d3bb3a496ca6e7bd
EvanMC <evancole82590@gmail.com>
2015-01-24 12:22:55 -0500

    First project commit
    Created a simple main file that prints a message with the version number

---
+++ GameServer/GameServer/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+const std::string version_number = "0.0.1";
+
+int main()
+{
+	std::cout << "Initial Game Server [version: " << version_number << "]" << std::endl;
+
+	system("PAUSE");
+	return 0;
+}
