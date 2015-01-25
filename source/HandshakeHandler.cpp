1c5ace836c34f8c976ddd84352994fb1d6dff359
EvanMC <evancole82590@gmail.com>
2015-01-24 21:33:47 -0500

    New HandshakeHandler function: explode()
    Created HandshakeHandler::explode() function

--- source/HandshakeHandler.cpp
+++ source/HandshakeHandler.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <string>
 #include "HandshakeHandler.h"
 
 HandshakeHandler::HandshakeHandler()
@@ -12,7 +10,7 @@ HandshakeHandler::~HandshakeHandler()
 	// Destructor
 }
 
-//
+// Trims whitespaces at front and end of string
 std::string HandshakeHandler::trim(std::string str)
 {
 	char* whitespace = " \t\n\r";
@@ -21,19 +19,43 @@ std::string HandshakeHandler::trim(std::string str)
 	std::string::size_type fpos = str.find_last_not_of(whitespace); 
 
 	// Was fpos found?
-	if (fpos != std::string::npos)
-	{
+	if (fpos != std::string::npos) {
 		str.erase(fpos + 1); // Erase trailing whitespace
-
 		// Searches the string for the first character that does not match any of the characters specified in its arguments.
 		fpos = str.find_first_not_of(whitespace);
-		
 		// Was fpos found?
-		if (fpos != std::string::npos)
-		{
+		if (fpos != std::string::npos) {
 			str.erase(0, fpos); // Erase leading whitespace
 		}
 	}
 
 	return str;
+}
+
+// Explodes a string into separate strings by a delimiter
+std::vector<std::string> HandshakeHandler::explode(const std::string& str, char del)
+{
+	std::string next;
+	std::vector<std::string> result;
+
+	// Iterate through each character of the string
+	for (std::string::const_iterator it = str.begin(); it != str.end(); it++) {
+		// If we've hit the delimiting character
+		if (*it == del) {
+			// If we have some characters collected, then add to result vector
+			if (!next.empty()) {
+				result.push_back(next);
+				next.clear();
+			}
+		} else {
+			// Collect the next character into the sequence
+			next += *it;
+		}
+	}
+
+	if (!next.empty()) {
+		result.push_back(next);
+	}
+
+	return result;
 }
--- source/HandshakeHandler.h
+++ source/HandshakeHandler.h
@@ -1,15 +1,16 @@
 // Handshake Handler
 //
-// The role for this class is to handle the handshake when it is sent
-// by the client when the connection opens.  You can read more about 
+// The role for this class is to handle the handshake that it is sent
+// from the client when the connection opens.  You can read more about 
 // the server-side responsibilies of the handshake in the link below:
-// https ://tools.ietf.org/html/rfc6455#section-4.2.1
+// https://tools.ietf.org/html/rfc6455#section-4.2.1
 
 #ifndef HANDSHAKEHANDLER_H
 #define HANDSHAKEHANDLER_H
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 class HandshakeHandler
 {
@@ -18,6 +19,7 @@ class HandshakeHandler
 	~HandshakeHandler();
 
 	std::string trim(std::string);
+	std::vector<std::string> explode(const std::string&, char);
 
 	private:
 	std::string resource;
